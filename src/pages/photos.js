593bda73c34dbe1ee8591fd97a91f957c63651f0
Siham <siham.hadi@live.se>
2019-12-21 16:17:45 +0100

    Changed to functional components

--- src/pages/photos.js
+++ src/pages/photos.js
@@ -1,45 +1,41 @@
-import React, { Component } from "react";
+import React from "react";
 import Head from "next/head";
 import "../styles/main.scss";
 import Navbar from "../components/navigation";
 import Footer from "../components/footer";
 import Layout from "../components/layout";
 
-export default class Photos extends Component {
-  render() {
-    return (
-      <Layout>
-        <div>
-          <Navbar />
-          <Head>
-            <title>Selection of photos</title>
-            <meta charSet="utf-8" />
-            <meta name="description" content="Photos taken by Siham Hadi" />
-          </Head>
-          <h5>Siham Hadi</h5>
+const Unit = () => {
+  return (
+    <Layout>
+      <div>
+        <Navbar />
+        <Head>
+          <title>Selection of photos</title>
+          <meta charSet="utf-8" />
+          <meta name="description" content="Photos taken by Siham Hadi" />
+        </Head>
+        <h5>Siham Hadi</h5>
 
-          <div className="container container-photos ">
-            <img src="./images/photos/landskap_srh.jpg" alt="landscape"></img>
-            <img src="./images/photos/landskap_srh_1.jpg" alt="landscape"></img>
-            <img
-              src="./images/photos/tagstation_srh_2.jpg"
-              alt="landscape"
-            ></img>
-            <img src="./images/photos/tagstation_srh_1.jpg" alt="station"></img>
-            <img src="./images/photos/tagstation_srh.jpg" alt="station"></img>
-            <img src="./images/photos/host_srh.jpg" alt="landscape"></img>
-            <img src="./images/photos/srh1.jpg" alt="skyline"></img>
-            <img src="./images/photos/srh2.jpg" alt="skyline"></img>
-            <img src="./images/photos/srh3.jpg" alt="skyline"></img>
-            <img src="./images/photos/srh4.jpg" alt="landscape"></img>
-            <img src="./images/photos/srh5.jpg" alt="landscape"></img>
-            <img src="./images/photos/srh6.jpg" alt="boxingring"></img>
-            <img src="./images/photos/srh7.jpg" alt="landscape"></img>
-            <img src="./images/photos/srh8.jpg" alt="landscape"></img>
-          </div>
-          <Footer />
+        <div className="container container-photos ">
+          <img src="./images/photos/landskap_srh.jpg" alt="landscape"></img>
+          <img src="./images/photos/landskap_srh_1.jpg" alt="landscape"></img>
+          <img src="./images/photos/tagstation_srh_2.jpg" alt="landscape"></img>
+          <img src="./images/photos/tagstation_srh_1.jpg" alt="station"></img>
+          <img src="./images/photos/tagstation_srh.jpg" alt="station"></img>
+          <img src="./images/photos/host_srh.jpg" alt="landscape"></img>
+          <img src="./images/photos/srh1.jpg" alt="skyline"></img>
+          <img src="./images/photos/srh2.jpg" alt="skyline"></img>
+          <img src="./images/photos/srh3.jpg" alt="skyline"></img>
+          <img src="./images/photos/srh4.jpg" alt="landscape"></img>
+          <img src="./images/photos/srh5.jpg" alt="landscape"></img>
+          <img src="./images/photos/srh6.jpg" alt="boxingring"></img>
+          <img src="./images/photos/srh7.jpg" alt="landscape"></img>
+          <img src="./images/photos/srh8.jpg" alt="landscape"></img>
         </div>
-      </Layout>
-    );
-  }
-}
+        <Footer />
+      </div>
+    </Layout>
+  );
+};
+export default Unit;
--- src/pages/wore.js
+++ src/pages/wore.js
@@ -3,45 +3,44 @@ import Head from "next/head";
 import "../styles/main.scss";
 import Navbar from "../components/navigation";
 import Layout from "../components/layout";
-export default class Wore extends Component {
-  render() {
-    return (
-      <Layout>
-        <div>
-          <Navbar />
-          <Head>
-            <title>Wore</title>
-            <meta charSet="utf-8" />
-            <meta
-              name="description"
-              content="Modest fashion from the high street shops"
-            />
-          </Head>
-          <h5>Project year: 2017 | Designer | Developer </h5>
-          <div className="container container-wore">
-            <p>
-              “Wore started out from a problem I had whilst shopping. Finding
-              modest clothes at the high street shops was not easy, so I started
-              curating all the clothes I found modest. Then I designed and built
-              Wore.”{"\n"}- Siham Hadi{" "}
-            </p>
-            <img src="./images/wore/wore1.jpg" alt="website"></img>
-            <img src="./images/wore/wore2.jpg" alt="website"></img>
-            <p>
-              The website was place for women to find inspiration on how they
-              could dress modestly.{" "}
-            </p>
-            <img src="./images/wore/wore3.jpg" alt="website"></img>{" "}
-            <img src="./images/wore/wore4.jpg" alt="website"></img>{" "}
-            <img src="./images/wore/wore5.jpg" alt="website"></img>
-            <img src="./images/wore/wore6.jpg" alt="website"></img>
-            <img src="./images/wore/wore7.jpg" alt="website"></img>
-            <img src="./images/wore/wore8.jpg" alt="website"></img>
-            <img src="./images/wore/wore9.jpg" alt="website"></img>
-            <img src="./images/wore/wore10.jpg" alt="website"></img>
-          </div>
+const Wore = () => {
+  return (
+    <Layout>
+      <div>
+        <Navbar />
+        <Head>
+          <title>Wore</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="description"
+            content="Modest fashion from the high street shops"
+          />
+        </Head>
+        <h5>Project year: 2017 | Designer | Developer </h5>
+        <div className="container container-wore">
+          <p>
+            “Wore started out from a problem I had whilst shopping. Finding
+            modest clothes at the high street shops was not easy, so I started
+            curating all the clothes I found modest. Then I designed and built
+            Wore.”{"\n"}- Siham Hadi{" "}
+          </p>
+          <img src="./images/wore/wore1.jpg" alt="website"></img>
+          <img src="./images/wore/wore2.jpg" alt="website"></img>
+          <p>
+            The website was place for women to find inspiration on how they
+            could dress modestly.{" "}
+          </p>
+          <img src="./images/wore/wore3.jpg" alt="website"></img>{" "}
+          <img src="./images/wore/wore4.jpg" alt="website"></img>{" "}
+          <img src="./images/wore/wore5.jpg" alt="website"></img>
+          <img src="./images/wore/wore6.jpg" alt="website"></img>
+          <img src="./images/wore/wore7.jpg" alt="website"></img>
+          <img src="./images/wore/wore8.jpg" alt="website"></img>
+          <img src="./images/wore/wore9.jpg" alt="website"></img>
+          <img src="./images/wore/wore10.jpg" alt="website"></img>
         </div>
-      </Layout>
-    );
-  }
-}
+      </div>
+    </Layout>
+  );
+};
+export default Wore;
