977c1d8e0939e93bfeb211fa56bb64bf210c93a3
Siham <siham.hadi@live.se>
2020-02-14 23:12:37 +0100

    about me updt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Head from "next/head";
-import ProjectsView from "./project";
-import Navbar from "../components/navigation";
-import Footer from "../components/footer";
-import "../styles/main.scss";
-import Layout from "../components/layout";
+import React from 'react';
+import Head from 'next/head';
+import ProjectsView from './project';
+import Navbar from '../components/navigation';
+import Footer from '../components/footer';
+import '../styles/main.scss';
+import Layout from '../components/layout';
 
 const Home = () => {
   return (
@@ -12,42 +12,42 @@ const Home = () => {
       <Layout>
         <Head>
           <title>Siham Hadi</title>
-          <link rel="icon" href="favicon.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta charSet="utf-8" />
+          <link rel='icon' href='favicon.ico' type='image/x-icon' />
+          <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+          <link rel='manifest' href='/manifest.json' />
+          <meta charSet='utf-8' />
           <meta
-            name="description"
-            content="Siham Hadi Software Developer And Designer Based In Stockholm."
+            name='description'
+            content='Siham Hadi Software Developer And Designer Based In Stockholm.'
           />
-          <meta name="keywords" content="JavaScript,Frontend,developer" />
-          <meta name="author" content="Siham Hadi" />
+          <meta name='keywords' content='JavaScript,Frontend,developer' />
+          <meta name='author' content='Siham Hadi' />
           <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            name='viewport'
+            content='width=device-width, initial-scale=1.0'
           />
         </Head>
         <Navbar />
         <main>
-          <div className="about">
-            <div className="about-me">
+          <div className='about'>
+            <div className='about-me'>
               <h1>
                 I have learnt to use a set of tools that I now try to create
                 value with.
               </h1>
               <p>
                 I’m Siham a software developer and designer based in Stockholm,
-                Sweden.{"\n"} I specialize in JavaScript. I have experience
-                working C ++, Swift and write some scripts in Python.{"\n"}
+                Sweden.{'\n'} I specialize in JavaScript. I have experience
+                working with C ++ and Python.{'\n'}
                 Speak: Somali - Swedish - English
               </p>
 
-              <span className="social">
-                <a href="mailto: siham.hadi@live.se">Email</a>
-                <a href="https://github.com/csobr" target="_blank">
+              <span className='social'>
+                <a href='mailto: siham.hadi@live.se'>Email</a>
+                <a href='https://github.com/csobr' target='_blank'>
                   Github
                 </a>
-                <a href="https://www.instagram.com/texturlab/" target="_blank">
+                <a href='https://www.instagram.com/texturlab/' target='_blank'>
                   Instagram
                 </a>
               </span>
