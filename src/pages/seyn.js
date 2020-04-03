272797a8ee998582c1497f94948236b83b57cdbc
Siham <siham.hadi@live.se>
2020-04-03 11:16:43 +0200

    New font

--- src/pages/seyn.js
+++ src/pages/seyn.js
@@ -18,7 +18,9 @@ export default class Seyn extends Component {
           <main>
             <Navbar />
             <h4>Seyn</h4>
-            <h5>Project year: 2020 | Designer | Developer | React Native</h5>
+            <h5>
+              Project year: 2020 | Creator | Designer | Developer | React Native
+            </h5>
             <p className='app-text'>Under construction. </p>
             <div className=' container container-app'>
               <img src='./images/seyn/seyn.png' alt='seyn'></img>
