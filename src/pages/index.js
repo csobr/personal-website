db6cc9825fa301080d85da7fb2727cc0a15b8e3b
csobr <siham.hadi@live.se>
2020-11-26 14:24:46 +0100

    Updated info, and dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,74 @@
-import React, { useEffect, useRef } from "react";
-import Head from "next/head";
-import Navbar from "../components/navigation";
-import Footer from "../components/footer";
-import "../styles/main.scss";
-import Layout from "../components/layout";
+import React, { useEffect, useRef } from 'react'
+import Head from 'next/head'
+import Navbar from '../components/navigation'
+import Footer from '../components/footer'
+import '../styles/main.scss'
+import Layout from '../components/layout'
 
 const Home = () => {
-  return (
-    <div>
-      <Layout>
-        <Head>
-          <title>Siham Hadi</title>
-          <link rel="icon" href="favicon.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="manifest" href="/manifest.json" />
-          <meta charSet="utf-8" />
-          <meta
-            name="description"
-            content="Siham Hadi Software Developer And Designer Based In Stockholm."
-          />
-          <meta name="keywords" content="JavaScript,Frontend,developer" />
-          <meta name="author" content="Siham Hadi" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-        </Head>
-        <Navbar />
-        <main>
-          <div className="about">
-            <div className="about-me">
-              <h1>Be curious and go deeper.</h1>
-              <p>
-                I’m Siham a software developer and designer based in Stockholm,
-                Sweden.{"\n"}I specialize in JavaScript. Language agnostic, I
-                learn the tools required to build a product.
-                {"\n"}Languages: Somali, Swedish and English
-              </p>
+    return (
+        <div>
+            <Layout>
+                <Head>
+                    <title>Siham Hadi</title>
+                    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+                    <link
+                        rel="shortcut icon"
+                        href="/favicon.ico"
+                        type="image/x-icon"
+                    />
+                    <link rel="manifest" href="/manifest.json" />
+                    <meta charSet="utf-8" />
+                    <meta
+                        name="description"
+                        content="Siham Hadi Software Developer And Designer Based In Stockholm."
+                    />
+                    <meta
+                        name="keywords"
+                        content="JavaScript,Frontend,developer"
+                    />
+                    <meta name="author" content="Siham Hadi" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0"
+                    />
+                </Head>
+                <Navbar />
+                <main>
+                    <div className="about">
+                        <div className="about-me">
+                            <h1>Be curious and go deeper.</h1>
+                            <p>
+                                I’m Siham a software developer and designer
+                                based in Stockholm, Sweden. I specialize in
+                                JavaScript. I learn the tools required to build
+                                a product.
+                                {'\n'}Languages: Somali, Swedish and English
+                            </p>
 
-              <span className="social">
-                <a href="mailto: siham.hadi@live.se">Email</a>
-                <a href="https://github.com/csobr" target="_blank">
-                  Github
-                </a>
-                <a href="https://www.instagram.com/texturlab/" target="_blank">
-                  Instagram
-                </a>
-              </span>
-            </div>
-          </div>
-        </main>
+                            <span className="social">
+                                <a href="mailto: siham.hadi@live.se">Email</a>
+                                <a
+                                    href="https://github.com/csobr"
+                                    target="_blank"
+                                >
+                                    Github
+                                </a>
+                                <a
+                                    href="https://www.instagram.com/texturlab/"
+                                    target="_blank"
+                                >
+                                    Instagram
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </main>
 
-        <Footer />
-      </Layout>
-    </div>
-  );
-};
+                <Footer />
+            </Layout>
+        </div>
+    )
+}
 
-export default Home;
+export default Home
--- src/pages/seyn.js
+++ src/pages/seyn.js
@@ -1,30 +1,32 @@
-import React, { Component } from "react";
-import "../styles/main.scss";
-import Head from "next/head";
-import Navbar from "../components/navigation";
-import Footer from "../components/footer";
-import Layout from "../components/layout";
+import React, { Component } from 'react'
+import '../styles/main.scss'
+import Head from 'next/head'
+import Navbar from '../components/navigation'
+import Footer from '../components/footer'
+import Layout from '../components/layout'
 
 export default class Seyn extends Component {
-  render() {
-    return (
-      <Layout>
-        <Head>
-          <title>Seyn</title>
-          <meta charSet='utf-8' />
-          <meta name='description' content='Seyn app' />
-        </Head>
-        <main>
-          <Navbar />
-          <h4>Seyn</h4>
-          <h5>Project year: 2020 | Designer | Developer </h5>
-          <p className='app-text'>Building </p>
-          <div className=' container container-app'>
-            <img src='./images/seyn/seyn.png' alt='seyn'></img>
-          </div>
-        </main>
-        <Footer />
-      </Layout>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <Head>
+                    <title>Seyn</title>
+                    <meta charSet="utf-8" />
+                    <meta name="description" content="Seyn app" />
+                </Head>
+                <main>
+                    <Navbar />
+                    <h4>Seyn</h4>
+                    <h5>
+                        Project year: 2020 | Designer | Developer | Native app{' '}
+                    </h5>
+                    <p className="app-text">Building </p>
+                    <div className=" container container-app">
+                        <img src="./images/seyn/seyn.png" alt="seyn"></img>
+                    </div>
+                </main>
+                <Footer />
+            </Layout>
+        )
+    }
 }
