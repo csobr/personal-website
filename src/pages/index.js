3dea7709908e6ecde5db8d28c972d63578463670
csobr <siham.hadi@live.se>
2020-12-15 19:40:48 +0100

    intro text, logo size

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ const Home = () => {
                                 I’m Siham a software developer and designer
                                 based in Stockholm, Sweden.{'\n'} I specialize
                                 in JavaScript but the language depends on the
-                                project requirements. {'\n'}
+                                project. {'\n'}
                             </p>
 
                             <span className="social">
