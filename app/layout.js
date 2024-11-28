f5516c8eabdee84e11cc4892553a71f6793ceb58
Siham Hadi <siham.hadi@live.se>
2024-11-28 06:07:50 +0100

    Update metadata in layout file

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,28 @@
 import Head from 'next/head';
 import '../index.css';
 
+export const metadata = {
+  title: 'Siham Hadi',
+  description: 'A Software Developer And Designer.',
+  keywords:
+    'Siham Hadi, JavaScript, Frontend, developer, Designer, Software Developer',
+  author: 'Siham Hadi',
+  viewport: 'width=device-width, initial-scale=1.0',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <title>Siham Hadi</title>
+        <title>{metadata.title}</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="manifest" href="/manifest.json" />
         <meta charSet="utf-8" />
-        <meta
-          name="description"
-          content="Siham Hadi | A Software Developer And Designer."
-        />
-        <meta name="keywords" content="JavaScript,Frontend,developer" />
-        <meta name="author" content="Siham Hadi" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <meta name="keywords" content={metadata.keywords} />
+        <meta name="author" content={metadata.author} />
+        <meta name="viewport" content={metadata.viewport} />
       </Head>
       <body>{children}</body>
     </html>
